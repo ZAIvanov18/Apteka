2b6365e12fabbdd07dde36c20d9c91b8f63e36b4
ZAIvanov18 <ZAIvanov18@codingburgas.bg>
2022-12-12 13:57:52 +0200

    Fix errors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Medicine med1 = new Medicine("paracetamol", 10, 20, LocalDate.of(1999, 03, 28));
+    Medicine med2 = new Medicine("paracetamol", 10, 20, LocalDate.of(2026,03, 28));
+    Medicine med3 = new Medicine("paracetamol", 10, 20, LocalDate.of(2020,03,28));
+        List<Medicine> meds = Arrays.asList(med1, med2, med3);
+        System.out.println();
+        Medicine.showMedicineThatCostsMoreThan5(meds);
+        Medicine.showMedicineThatHaveLessThan10(meds);
+        Medicine.showExpiredMedicine(meds, LocalDate.of(2022, 12, 12));
     }
 }
--- src/Medicine.java
+++ src/Medicine.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Medicine {
+import java.time.LocalDate;
+import java.util.List;
+
+public class Medicine {
+    String name;
+    double price;
+    int inStock;
+    LocalDate date;
+
+    public Medicine(String name, double price, int inStock, LocalDate date) {
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+        this.date = date;
+    }
+
+    public static void showMedicineThatCostsMoreThan5(List<Medicine> meds){
+        for (int i = 0; i < meds.size(); i++) {
+            if (meds.get(i).price >= 5.00) {
+                System.out.println(meds.get(i));
+            }
+        }
+    }
+
+    public static void showMedicineThatHaveLessThan10(List<Medicine> meds){
+        for (int i = 0; i < meds.size(); i++) {
+            if (meds.get(i).inStock>10){
+                System.out.println(meds.get(i));
+            }
+
+        }
+    }
+
+    public static void showExpiredMedicine(List<Medicine> meds, LocalDate currentDate){
+        for (int i = 0; i < meds.size(); i++) {
+            if (meds.get(i).date.isAfter(currentDate)){
+                System.out.println(meds.get(i).name);
+            }
+
+        }
+    }
+
 }
